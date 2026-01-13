6746fc59212bc247440fb33b72985bf9813327d3
harshmehta162005-max <harshmehta162005@gmail.com>
2026-01-13 14:37:36 +0530

    fixed

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -33,8 +33,8 @@ const registerUser = async (req, res) => {
       
       res.cookie('jwt', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV !== 'development', 
-        sameSite: 'strict',
+        secure: true, 
+        sameSite: none,
         maxAge: 30 * 24 * 60 * 60 * 1000, 
       });
 
